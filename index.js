b5b8f0ad621d7c9f0f8261bce33535825df5293b
Mora <rodrigomoralejas@gmail.com>
2022-07-05 23:53:26 -0300

    modificaciones y finalizacion de la app

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 alert("Elija un numero, tire un dado y pruebe su suerte. Presione ENTER para continuar")
 
-let numeroRandom = Math.floor(Math.random() * 6 + 1)
+//Inicio de app
+const inicioJuego = () => {
 
-const lanzarDados = () => {
-    let pedidoNumero = parseInt(prompt("Escoja un numero del 1 al 6"))
-    
-    
-    switch (true) {
-        case pedidoNumero > 0 && pedidoNumero < 7:
-            alert("Presione ENTER para lanzar el dado")
-            if (pedidoNumero === numeroRandom) {
-                alert("Ganaste")
+    //Generacion de numero random
+    let numeroRandom = Math.floor(Math.random() * 6 + 1)
+
+    //Peticion de numeros y lanzamiento de dado
+    const lanzarDados = () => {
+        let pedidoNumero = parseInt(prompt("Escoja un numero del 1 al 6"))
+
+        const playAgain = () => {
+            let respuestaUsuario = prompt("Desea seguir jugando? SI o NO").toUpperCase()
+
+            if (respuestaUsuario === "SI") {
+                inicioJuego()
+            } else if (respuestaUsuario === "NO") {
+                alert("Gracias vuelvas prontos")
             } else {
-                alert(`Perdiste, el numero que salio es ${numeroRandom}`)
+                alert("Respuesta no valida")
+                playAgain()
             }
-            break;
+        }
 
-        case pedidoNumero === 0 || pedidoNumero > 6:
-            alert("Ingrese un numero entre 1 y 6")
-            lanzarDados()
-            break;
+        //Evalua numero elegido con el numero random
+        switch (true) {
+            case pedidoNumero > 0 && pedidoNumero < 7:
+                alert("Presione ENTER para lanzar el dado")
+                if (pedidoNumero === numeroRandom) {
+                    alert("Ganaste")
+                    playAgain()
+                } else {
+                    alert(`Perdiste, el numero que salio es ${numeroRandom}`)
+                    playAgain()
+                }
+                break;
 
-        default:
-            alert("Caracter no valido, intente de nuevo")
-            lanzarDados()
-            break;
+            case pedidoNumero === 0 || pedidoNumero > 6:
+                alert("Ingrese un numero entre 1 y 6")
+                lanzarDados()
+                break;
+
+            default:
+                alert("Caracter no valido, intente de nuevo")
+                lanzarDados()
+                break;
+        }
     }
+
+    lanzarDados()
 }
-lanzarDados()
+//Funcion que inicia el juego
+inicioJuego()
