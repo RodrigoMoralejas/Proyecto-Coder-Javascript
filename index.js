980edc93f46e0ef858fb50de4436a04f5593851f
Mora <rodrigomoralejas@gmail.com>
2022-07-28 20:49:24 -0300

    2da entrega

--- index.js
+++ index.js
@@ -1,65 +1,137 @@
-alert("Bienvenido al baul de items, ingrese sus items")
+const storeList = document.getElementById("storeList")
+const vaultItems = document.getElementById("vaultItems")
 
-const baul = []
+const items = [{
+        id: "sword",
+        price: 20,
+        img: "./img/swordCard.png"
+    },
 
-const contenidoBaul = []
+    {
+        id: "axe",
+        price: 25,
+        img: "./img/axeCard.png"
+    },
 
-const mainDiv = document.getElementById("main")
+    {
+        id: "bow",
+        price: 30,
+        img: "./img/bowCard.png"
+    },
 
-let parrafosHijos
+    {
+        id: "wand",
+        price: 30,
+        img: "./img/wandCard.webp"
+    },
 
-//Funcion que inicia la app
-const iniciarApp = () => {
+    {
+        id: "helmet",
+        price: 10,
+        img: "./img/helmetCard.jpg"
+    },
 
-    const mostrarBaul = () => {
-        for (let i = 0; i < baul.length; i++) {
-            const itemDiv = document.createElement("p")
-            itemDiv.classList.add("parrafo")
-            itemDiv.innerHTML = `${baul[i].item} ${baul[i].cantidad}`
-            mainDiv.append(itemDiv)
-        }
+    {
+        id: "armor",
+        price: 60,
+        img: "./img/armorCard.jpg"
+    },
 
-        parrafosHijos = document.querySelectorAll("p")
+    {
+        id: "pants",
+        price: 40,
+        img: "./img/pantsCard.jpg"
+    },
+
+    {
+        id: "boots",
+        price: 20,
+        img: "./img/bootsCard.jpg"
+    },
+
+    {
+        id: "gloves",
+        price: 10,
+        img: "./img/glovesCard.jpg"
+    },
+
+    {
+        id: "potion",
+        price: 5,
+        img: "./img/potionCard.png"
+    },
+]
+
+let userItem = []
+
+const userVault = () => {
+    userItem.forEach((itemV) => {
+        console.log(userItem)
+        console.log(itemV)
+        const newItem = document.createElement("div")
+        newItem.classList.add("itemVault")
+        newItem.id = itemV.id
+        newItem.innerHTML = `
+        <div class="divImg">
+            <img class="vaultImg" src="${itemV.img}" alt="">
+        </div>
+        <h2>${itemV.id}</h2>`
+        vaultItems.append(newItem)
+    })}
+
+const storageChecker = () => {
+    if (localStorage.getItem("userItemsDB")) {
+        userItem = JSON.parse(localStorage.getItem("userItemsDB"))
+        userVault()
+    } else {
+        localStorage.setItem("userItemsDB", userItem)
     }
-
-    const pedirOtroItem = () => {
-        let otroItem = prompt("Desea ingresar otro item? SI/NO").toUpperCase()
-        if (otroItem === "SI") {
-            iniciarApp()
-        } else if (otroItem === "NO") {
-            alert("Adios")
-        } else {
-            alert("Ingrese respuesta valida")
-            pedirOtroItem()
-        }
-    }
-
-    const pedirItem = () => {
-        let itemUsuario = prompt("Ingrese su item: ")
-        let itemCantidad = parseInt(prompt("Ingrese la cantidad de items a dejar"))
-        baul.push({
-            item: itemUsuario,
-            cantidad: itemCantidad
-        })
-        pedirOtroItem()
-    }
-    pedirItem()
-
-    //Boton mostrar items
-    const botonMostrar = document.getElementById("boton")
-    
-    botonMostrar.addEventListener("click", () => {
-        if (mainDiv.children.length === 0) {
-            mostrarBaul()
-        }
-        if(mainDiv.children.length >= 1) {
-            for(let i = 0; i < parrafosHijos.length; i++) {
-                parrafosHijos[i].classList.toggle("parrafo")
-                parrafosHijos[i].classList.toggle("parrafo2")
-            }
-        }
-    })
 }
 
-//Inicio de app
-iniciarApp()
+storageChecker()
+
+items.forEach((item) => {
+    console.log(item)
+    const newItem = document.createElement("div")
+    newItem.classList.add("storeItem")
+    newItem.id = item.id
+    newItem.innerHTML = `
+        <div class="itemImg">
+            <img class="cardImg" src="${item.img}" alt="">
+        </div>
+        <h3>${item.id}</h3>
+        <p>${item.price}</p>
+    `
+    storeList.append(newItem)
+})
+
+storeList.addEventListener("click", (e) => {
+    const selectedItem = e.target
+
+    if (selectedItem.classList.contains("storeItem")) {
+
+        const itemFind = items.find(x => x.id === selectedItem.id)
+
+        console.log(itemFind)
+
+        userItem.push({
+            id: itemFind.id,
+            price: itemFind.price,
+            img: itemFind.img
+        })
+
+        console.log(userItem)
+
+        const itemVault = document.createElement("div")
+        itemVault.classList.add("itemVault")
+        itemVault.innerHTML = `
+                <div class="divImg">
+                <img class="vaultImg" src="${itemFind.img}" alt="">
+                </div>
+                <h2>${selectedItem.id}</h2>
+            `
+        vaultItems.append(itemVault)
+
+        localStorage.setItem("userItemsDB", JSON.stringify(userItem))
+    }
+})
