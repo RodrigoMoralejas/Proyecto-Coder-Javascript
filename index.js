3451d25d3379cc08590bf0945d32e3b831a4f7f9
Mora <rodrigomoralejas@gmail.com>
2022-09-02 18:37:26 -0300

    fix2

--- index.js
+++ index.js
@@ -11,71 +11,77 @@ localStorage.getItem("usersDB")
   ? (userLogin = JSON.parse(localStorage.getItem("usersDB")))
   : localStorage.setItem("usersDB", userLogin);
 
-formLogin.addEventListener("submit", (e) => {
-  e.preventDefault();
+const userActive = userLogin.find((x) => x.logged === true);
 
-  const nameInput = nameLogin.value;
-  const passInput = passLogin.value;
+if (userActive !== undefined) {
+  window.location.href = "./app.html";
+} else {
+  formLogin.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  const userSearch = userLogin.find((x) => x.user === nameInput);
+    const nameInput = nameLogin.value;
+    const passInput = passLogin.value;
 
-  if (nameInput.trim() === "" || passInput.trim() === "") {
-    Swal.fire({
-      icon: "error",
-      title: "Empty fields",
-      text: "You need to put name and pass",
-    });
-  } else if (userSearch !== undefined && userSearch.pass !== passInput){
-    Swal.fire({
-      icon: "error",
-      title: "Wrong password",
-      text: "Password incorrect, please try again",
-    });
-  } else if (userSearch !== undefined && userSearch.pass === passInput) {
-    userSearch.logged = true;
-    localStorage.setItem("usersDB", JSON.stringify(userLogin));
-    window.location.href = "./app.html";
-  } else {
-    Swal.fire({
-      icon: "warning",
-      title: "Attention",
-      text: "This user does not exists, click to create one",
-    });
-  }
-});
+    const userSearch = userLogin.find((x) => x.user === nameInput);
 
-btnCreate.addEventListener("click", () => {
-  const nameInput = nameLogin.value;
-  const passInput = passLogin.value;
+    if (nameInput.trim() === "" || passInput.trim() === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Empty fields",
+        text: "You need to put name and pass",
+      });
+    } else if (userSearch !== undefined && userSearch.pass !== passInput) {
+      Swal.fire({
+        icon: "error",
+        title: "Wrong password",
+        text: "Password incorrect, please try again",
+      });
+    } else if (userSearch !== undefined && userSearch.pass === passInput) {
+      userSearch.logged = true;
+      localStorage.setItem("usersDB", JSON.stringify(userLogin));
+      window.location.href = "./app.html";
+    } else {
+      Swal.fire({
+        icon: "warning",
+        title: "Attention",
+        text: "This user does not exists, click to create one",
+      });
+    }
+  });
 
-  const userFind = userLogin.find((x) => x.user === nameInput);
+  btnCreate.addEventListener("click", () => {
+    const nameInput = nameLogin.value;
+    const passInput = passLogin.value;
 
-  if (nameInput.trim() === "" || passInput.trim() === "") {
-    Swal.fire({
-      icon: "error",
-      title: "Empty fields",
-      text: "You need to put name and pass",
-    });
-  } else if (userFind !== undefined) {
-    Swal.fire({
-      icon: "info",
-      title: "Attention",
-      text: "User already exists",
-    });
-  } else {
-    userLogin.push({
-      user: nameInput,
-      pass: passInput,
-      coins: 500,
-      logged: false,
-      items: [],
-    });
-    Swal.fire({
-      icon: "success",
-      title: "Successful",
-      text: "User created",
-    });
+    const userFind = userLogin.find((x) => x.user === nameInput);
 
-    localStorage.setItem("usersDB", JSON.stringify(userLogin));
-  }
-});
+    if (nameInput.trim() === "" || passInput.trim() === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Empty fields",
+        text: "You need to put name and pass",
+      });
+    } else if (userFind !== undefined) {
+      Swal.fire({
+        icon: "info",
+        title: "Attention",
+        text: "User already exists",
+      });
+    } else {
+      userLogin.push({
+        user: nameInput,
+        pass: passInput,
+        coins: 500,
+        logged: false,
+        items: [],
+      });
+      Swal.fire({
+        icon: "success",
+        title: "Successful",
+        text: "User created",
+      });
+
+      localStorage.setItem("usersDB", JSON.stringify(userLogin));
+    }
+  });
+}
